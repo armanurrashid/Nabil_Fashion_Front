a2e67938642e6acda64f8d0d6880f162927fe83e
Armanur Rashid <armanurrashid105086@gmail.com>
2025-04-25 15:53:59 +0600

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Dashboard from './pages/dashboard'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Dashboard />}></Route>
+          <Route path="/home" exact element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="p-6">
+        <h1>Hello World</h1>
+        <p className="text-green-600 text-lg font-semibold mt-2">All is ok</p>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
